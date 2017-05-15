338ca1c2f41adcfd550ae7590d498e2a28c895ca
Jimmy.Jhang <t6820310@ntut.org.tw>
2017-05-15 22:37:33 +0800

    add more detail about service content

--- src/components/ServiceContent.vue
+++ src/components/ServiceContent.vue
@@ -7,7 +7,7 @@
     <div class="center-flex-items flex-wrap">
       <div class="column pad column-center-flex-item">
         <h2>企業內部培訓</h2>
-        <p>協助企業導入React.js + Node.js 的全端開發</p>
+        <p class="service-content-text">協助企業導入React.js + Node.js 全端開發。套用時下最流行的 Javascript 全端技術，讓培訓人員在<b><u>三個月</u></b>內學會前後端開發，無論是<b><u>IoT的即時監控或社群相關應用程式</u></b>都能夠駕馭自如。</p>
       </div>
       <div class="column pad column-center-flex-item">
         <img src="../assets/configuration.svg" class="content-img" />
@@ -17,7 +17,7 @@
     <div class="center-flex-items flex-wrap">
       <div class="column pad column-center-flex-item pull-left">
         <h2>影像辨識</h2>
-        <p class="description">協助企業使用Google Cloud Vision 進行影像處理</p>
+        <p class="description">協助企業使用 Google Cloud Vision 進行影像處理</p>
       </div>
       <div class="column pad column-center-flex-item">
         <img src="../assets/eye.svg"
@@ -43,6 +43,13 @@
   @import '../assets/common.scss';
   .service-content {
 
+  }
+  .service-content-text {
+    line-height: 30px;
+    text-align: left;
+  }
+  .pad {
+    padding: 20px 20px;
   }
   .description {
   }
